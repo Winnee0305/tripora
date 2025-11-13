7f8f4703353c9dfee73b0c451b3726daf7d73e75
Winnee0305 <winnee.chew@gmail.com>
2025-11-13 22:58:12 +0800

    Add dependencies for google maps

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -12,6 +12,7 @@ import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
 import 'package:flutter_image_compress_web/flutter_image_compress_web.dart';
 import 'package:flutter_native_splash/flutter_native_splash_web.dart';
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:permission_handler_html/permission_handler_html.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -25,6 +26,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   FirebaseStorageWeb.registerWith(registrar);
   FlutterImageCompressWeb.registerWith(registrar);
   FlutterNativeSplashWeb.registerWith(registrar);
+  GoogleMapsPlugin.registerWith(registrar);
   ImagePickerPlugin.registerWith(registrar);
   WebPermissionHandler.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
